4e075cf345571ec3d608e537afa4c349bde4577d
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-30 23:39:31 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%y-%m-%d-%h-%m-%s)
-
-LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+SCRIPT_NAME=$(basename "$0" .sh)
+TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
 mkdir -p $LOGS_FOLDER
 
@@ -12,24 +11,22 @@ USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
 Y="\e[33m"
+N="\e[0m"
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root priveleges" | tee -a $LOG_FILE
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]; then
+        echo -e "${R}Please run this script with root privileges${N}"
         exit 1
     fi
 }
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$R $2 is...FAILED $N" | tee -a $LOG_FILE
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 ... FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$G $2 is...SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$G $2 ... SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
@@ -37,22 +34,21 @@ CHECK_ROOT
 
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-dnf install mysql-server -y 
-VALIDATE $? "Installing Mysql-server"
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld 
-VALIDATE $? "Enabled Mysql-server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling MySQL Service"
 
-systemctl start mysqld 
-VALIDATE $? "started Mysql-sever"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting MySQL Service"
 
-mysql -h mysql.daws100s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+mysql -u root -pExpenseApp@1 -e "show databases;" &>>$LOG_FILE
 
-if [ $? -ne 0 ]
-then 
-    echo "MYSQL root password is not setup, setting now" &>>$LOG_FILE
-    mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "setting up root password"
+if [ $? -ne 0 ]; then
+    echo "MySQL root password not set. Setting now..." | tee -a $LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+    VALIDATE $? "Setting MySQL root password"
 else
-    echo -e "Mysql root password is already Setup...$Y skipping $N" | tee -a $LOG_FILE
+    echo -e "$Y MySQL root password already set... skipping $N" | tee -a $LOG_FILE
 fi
