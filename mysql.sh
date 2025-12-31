e928f4245bf6f839df6caeac58b8ca5d477a0eda
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-31 09:59:46 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -48,13 +48,13 @@ then
     systemctl start mysqld &>>$LOG_FILE
     VALIDATE $? "Starting MySQL Service"
 else
-    echo -e "Mysql is already $Y installed, nothing to do...$N"
+    echo -e "Mysql is already $Y installed, nothing to do...$N" | tee -a $LOG_FILE
 fi
 
 mysql -h mysql.daws100s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    echo "MySQL root password is not setup, setting now" | tee -a $LOG_FILE
     mysql_secure_installation --set-root-pass ExpenseApp@1
     VALIDATE $? "Setting UP root password"
 else
