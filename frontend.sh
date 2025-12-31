0990023a68c5d87c0be63ce1e7bd09e32bf9910f
mlnawsdevops <mlnpublicact365@gmail.com>
2025-12-31 13:35:59 +0530

    expense-shell

--- frontend.sh
+++ frontend.sh
@@ -16,7 +16,7 @@ USERID=$(id -u)
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then 
-        echo "Please run this script with root privileges"
+        echo "Please run this script with root privileges."
         exit 1
     fi
 }
@@ -34,27 +34,27 @@ VALIDATE(){
 
 echo "Script started execution at: $(date)" | tee -a $LOG_FILE
 
-dnf list installed nginx 
+dnf list installed nginx &>>$LOG_FILE
 
 if [ $? -ne 0 ]
 then
-    echo "Nginx is not installed, going to install it"
-    dnf install nginx -y
+    echo "Nginx is not installed, going to install it" | tee -a $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "Nginx installation"
 else
-    echo "Nginx is already installed, nothing to do...$Y SKIPPING $N"
+    echo "Nginx is already installed, nothing to do...$Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enablling nginx"
 
-systemctl start nginx
+systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removed files inside nginx path"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend application code"
 
 cd /usr/share/nginx/html
@@ -63,16 +63,19 @@ VALIDATE $? "Extracting frontend application code"
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 
-systemctl restart nginx
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting the nginx"
 
-netstat -lntp 
+systemctl status nginx &>>$LOG_FILE
+VALIDATE $? "Nginx status"
+
+netstat -lntp &>>$LOG_FILE
 VALIDATE $? "Frontend port 80 is running"
 
-telnet backend.daws100s.online 8080
+telnet backend.daws100s.online 8080 
 VALIDATE $? "Frontend is connected to Backend"
 
-ping backend.daws100s.online
+ping backend.daws100s.online &>>$LOG_FILE
 VALIDATE $? "Backend pinging"
 
 ps -ef | grep nginx
